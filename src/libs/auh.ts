eaa5afb55ece50cb09d9a6473eae2b16e7061f14
graffuko <sarah.p.graff@edu.sait.ca>
2024-04-17 09:59:48 -0600

    just some more database stuff

---
+++ src/libs/auh.ts
@@ -0,0 +1,21 @@
+import { NextAuthOptions } from 'next-auth';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import GithubProvider from 'next-auth/providers/github';
+
+import prisma from "./prisma";
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string,
+        }),
+    ],
+    session: {
+        strategy: 'jwt',
+    },
+    adapter: PrismaAdapter(prisma),
+    debug: process.env.NODE_ENV === 'development',
+    secret: process.env.NEXTAUTH_SECRET,
+    callbacks: {},
+};
