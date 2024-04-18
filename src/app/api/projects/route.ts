eab8461fb30950a79098e4d8316b212534a9d0aa
graffuko <sarah.p.graff@edu.sait.ca>
2024-04-18 14:28:35 -0600

    smol update

--- src/app/api/projects/route.ts
+++ src/app/api/projects/route.ts
@@ -24,3 +24,46 @@ export async function GET(req: Request, res: Response) {
     }
 }
 
+export async function POST(req: Request, res: Response) {
+    const session = await getServerSession(authOptions)
+
+    if(!session) {
+        return new NextResponse('Not Authenticated', { status: 500 });
+    }
+
+    const userEmail = session.user?.email;
+
+    if (!userEmail) {
+        return new NextResponse('Not Authenticated', { status: 500 });
+    }
+
+    const user = await prisma.user.findUnique({where: {email: userEmail}});
+
+    if (!user) return new NextResponse('Not Authenticated', { status: 500 });
+
+    const { description, name, slug } = await req.json();
+
+    if(!description || !name || !slug) {
+        return new NextResponse('All fields are required to be filled', { status: 400 });
+    }
+
+    try {
+        const createdProject = await prisma.project.create({
+            data: {
+                description,
+                name,
+                slug,
+                userId: user.id,
+            },
+        });
+
+        return NextResponse.json(createdProject, {
+            status: 200,
+            statusText: 'Project Created Successfully',
+        });
+    } catch (error) {
+        console.log(error);
+        return new NextResponse('Cannot create project', { status: 500 });
+    }
+
+}
