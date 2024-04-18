ab5894fe50601e40a95ea34a044edd51b482e200
graffuko <sarah.p.graff@edu.sait.ca>
2024-04-18 14:40:10 -0600

    correction on button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
   return (
     <>
       <button className='relative outline-none inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 hover:text-white focus:ring-blue-300'>
-        <span className='relative px-5 py-2.5 transition-all ease-in duration-75 bg-white rounded-md group-hover:b-opacity-0'>
+        <span className='relative px-5 py-2.5 transition-all ease-in duration-75 bg-white rounded-md group-hover:bg-opacity-0'>
           Create Project
           </span>
       </button>
